82d41cf22b3abba716dcdf66ca1857dfc273eb6a
Елистратов <elistratov-av@yandex.ru>
2025-03-31 23:02:52 +0300

    5.7. Армия моделей

--- models/User.php
+++ models/User.php
@@ -2,103 +2,143 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $name
+ * @property int $city_id
+ * @property string $password
+ * @property string $dt_add
+ * @property int $blocked
+ * @property string|null $last_activity
+ *
+ * @property City $city
+ * @property File[] $files
+ * @property Opinion[] $opinions
+ * @property Opinion[] $opinions0
+ * @property Reply[] $replies
+ * @property Category[] $categories
+ * @property UserSettings $userSettings
+ */
+class User extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
 
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentity($id)
+    public static function tableName()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return 'users';
     }
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function rules()
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return [
+            [['last_activity'], 'default', 'value' => null],
+            [['blocked'], 'default', 'value' => 0],
+            [['email', 'name', 'city_id', 'password'], 'required'],
+            [['city_id', 'blocked'], 'integer'],
+            [['dt_add', 'last_activity'], 'safe'],
+            [['email', 'name'], 'string', 'max' => 255],
+            [['password'], 'string', 'max' => 64],
+            [['email'], 'unique'],
+            [['city_id'], 'exist', 'skipOnError' => true, 'targetClass' => City::class, 'targetAttribute' => ['city_id' => 'id']],
+        ];
     }
 
     /**
-     * Finds user by username
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'name' => 'Name',
+            'city_id' => 'City ID',
+            'password' => 'Password',
+            'dt_add' => 'Dt Add',
+            'blocked' => 'Blocked',
+            'last_activity' => 'Last Activity',
+        ];
+    }
+
+    /**
+     * Gets query for [[City]].
      *
-     * @param string $username
-     * @return static|null
+     * @return \yii\db\ActiveQuery
      */
-    public static function findByUsername($username)
+    public function getCity()
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return $this->hasOne(City::class, ['id' => 'city_id']);
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
+     * Gets query for [[Files]].
      *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
+     * @return \yii\db\ActiveQuery
      */
-    public function validatePassword($password)
+    public function getFiles()
     {
-        return $this->password === $password;
+        return $this->hasMany(File::class, ['user_id' => 'id']);
     }
+
+    /**
+     * Gets query for [[Opinions]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOpinions()
+    {
+        return $this->hasMany(Opinion::class, ['owner_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Opinions0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOpinions0()
+    {
+        return $this->hasMany(Opinion::class, ['performer_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Replies]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReplies()
+    {
+        return $this->hasMany(Reply::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Categories]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategories()
+    {
+        return $this->hasMany(Category::class, ['id' => 'category_id'])->viaTable('user_categories', ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[UserSetting]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserSettings()
+    {
+        return $this->hasOne(UserSettings::class, ['user_id' => 'id']);
+    }
+
 }
---
+++ models/UserSettings.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_settings".
+ *
+ * @property int $id
+ * @property string|null $address
+ * @property string|null $bd
+ * @property string|null $avatar_path
+ * @property string|null $about
+ * @property string|null $phone
+ * @property string|null $skype
+ * @property string|null $messenger
+ * @property int|null $notify_new_msg
+ * @property int|null $notify_new_action
+ * @property int|null $notify_new_reply
+ * @property int|null $opt_hide_contacts
+ * @property int|null $opt_hide_me
+ * @property int|null $is_performer
+ * @property int $user_id
+ *
+ * @property Users $user
+ */
+class UserSettings extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_settings';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['address', 'bd', 'avatar_path', 'about', 'phone', 'skype', 'messenger'], 'default', 'value' => null],
+            [['is_performer'], 'default', 'value' => 0],
+            [['bd'], 'safe'],
+            [['about'], 'string'],
+            [['notify_new_msg', 'notify_new_action', 'notify_new_reply', 'opt_hide_contacts', 'opt_hide_me', 'is_performer', 'user_id'], 'integer'],
+            [['user_id'], 'required'],
+            [['address', 'avatar_path'], 'string', 'max' => 255],
+            [['phone'], 'string', 'max' => 11],
+            [['skype', 'messenger'], 'string', 'max' => 32],
+            [['user_id'], 'unique'],
+            [['phone', 'skype', 'messenger'], 'unique', 'targetAttribute' => ['phone', 'skype', 'messenger']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'address' => 'Address',
+            'bd' => 'Bd',
+            'avatar_path' => 'Avatar Path',
+            'about' => 'About',
+            'phone' => 'Phone',
+            'skype' => 'Skype',
+            'messenger' => 'Messenger',
+            'notify_new_msg' => 'Notify New Msg',
+            'notify_new_action' => 'Notify New Action',
+            'notify_new_reply' => 'Notify New Reply',
+            'opt_hide_contacts' => 'Opt Hide Contacts',
+            'opt_hide_me' => 'Opt Hide Me',
+            'is_performer' => 'Is Performer',
+            'user_id' => 'User ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+
+}
